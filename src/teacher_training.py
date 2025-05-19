ea2794877978b5478f0f48eb4f11f2aeb40d98fa
danielsoy <48926149+danielsoy@users.noreply.github.com>
2025-05-19 14:05:22 -0300

    Update teacher_training.py

--- src/teacher_training.py
+++ src/teacher_training.py
@@ -71,7 +71,8 @@ def train(args):
     teacher.to(device)
 
     # Loading saved model
-    model_name = f'../model/{args.dataset}/teacher_{args.patch_size}_net.pt'
+    model_name = f'/content/drive/MyDrive/data/ticher student models/{args.dataset}/teacher_{args.patch_size}_net.pt'    
+    
     load_model(teacher, model_name)
 
     # Define optimizer
